094f8329d7bebf3f42cddbc6c6f3d5463dac18ab
Rob Woodward <robwwd@gmail.com>
2018-04-23 09:23:07 +0000

    removed callback

--- Device-Utils/lib/Device/Utils.pm
+++ Device-Utils/lib/Device/Utils.pm
@@ -625,8 +625,7 @@ sub _setup_logging {
               facility  => $self->{'options'}->{'syslog'}->{$_}->{'facility'},
               severity  => $self->{'options'}->{'syslog'}->{$_}->{'severity'},
               host    => $self->{'options'}->{'syslog'}->{$_}->{'host'},
-              port    => $self->{'options'}->{'syslog'}->{$_}->{'port'},
-							callbacks => $self->_cut_timestamp
+              port    => $self->{'options'}->{'syslog'}->{$_}->{'port'}
               )
             );
     }
@@ -704,14 +703,6 @@ sub _setup_logging {
   ) unless $self->{'options'}->{'quiet'};
 
   return $log;
-}
-
-sub _cut_timestamp {
-	my $params = shift;
-
-	print Dumper ($params);
-
-	return $params->message;
 }
 
 sub _valid_line {
